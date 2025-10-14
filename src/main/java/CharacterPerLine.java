5983a10e0cd08ae2b784ccbf2cb498f81b2e4a6e
etilE-mI <treebro2005@gmail.com>
2025-10-14 13:58:44 -0400

    Update CharacterPerLine.java

--- src/main/java/CharacterPerLine.java
+++ src/main/java/CharacterPerLine.java
@@ -12,8 +12,7 @@ public class CharacterPerLine {
 
         for(int i=0; i<word.length(); i++)
         {
-            System.out.println(word.charAt(i));
+            System.out.print(word.charAt(i) + "\n");
         }
-
     }
 }
