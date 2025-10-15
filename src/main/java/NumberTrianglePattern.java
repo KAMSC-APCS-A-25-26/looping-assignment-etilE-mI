e2771ad454fc390a6147d8340bca7874db57922a
etilE-mI <treebro2005@gmail.com>
2025-10-15 19:26:28 -0400

    Update NumberTrianglePattern.java

--- src/main/java/NumberTrianglePattern.java
+++ src/main/java/NumberTrianglePattern.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class NumberTrianglePattern {
     public static void main(String[] args) {
-                
+
         // TODO: Step 1 - Get user input
 
-        
+
         // TODO: Step 2 - Generate the number triangle pattern
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("");
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+
+
+        
     }
 }
