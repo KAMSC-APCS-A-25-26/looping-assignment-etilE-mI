1917c337952df7f959537782c628bfdf32a5ec2c
etilE-mI <treebro2005@gmail.com>
2025-10-19 19:46:14 -0400

    Update SumOfEvenNumbers.java

--- src/main/java/SumOfEvenNumbers.java
+++ src/main/java/SumOfEvenNumbers.java
@@ -10,27 +10,19 @@ public class SumOfEvenNumbers {
         
         // TODO: Step 4 - Display the result
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter an integer value: ");
-        int num = input.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer value: ");
+        int num = sc.nextInt();
 
-        if (num < 2)
-        {
-            System.out.println("Error: Input value must be 2 or greater.");
+        if(num<2){
+            System.out.println("Error: Input must be 2 or greater.");
         }
-        else
-        {
+        else{
             int sum = 0;
-            System.out.print("Even numbers: ");
-            for (int i = 2; i <= num; i += 2) {
+            for (int i = 2; i <= num; i+=2) {
                 sum += i;
-                System.out.print(i);
-                if (i + 2 <= num)
-                {
-                    System.out.print(", ");
-                }
             }
-            System.out.println("\nThe sum of even numbers between 2 and " + num + " is " + sum + ".");
+            System.out.println("The sum of even numbers between 2 and " + num + " is " + sum + ".");
         }
 
     }
